1811262224d137aa5fe9fbb1df5f8f95c51dde56
Samnoeun <samnoeun.run@student.passerellesnumeriques.org>
2025-06-23 13:32:58 +0700

    Frontend Library Management

---
+++ src/views/BookView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import api from '../axios'  // adjust the path if needed
+
+const books = ref([])
+
+onMounted(async () => {
+  try {
+    const res = await api.get('/books')
+    books.value = res.data
+  } catch (error) {
+    console.error('Error fetching books:', error)
+  }
+})
+</script>
